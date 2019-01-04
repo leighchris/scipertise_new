9654cc8edab1b1416b6109e3677945437f0abb56
Leigh Christopher <leigh.christopher2@gmail.com>
2019-01-04 14:12:53 -0500

    second commit

--- scipertise_demo/settings.py
+++ scipertise_demo/settings.py
@@ -123,18 +123,14 @@ WSGI_APPLICATION = 'scipertise_demo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='DATABASE_URL'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-#
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
 
 
@@ -252,6 +248,11 @@ EMAIL_PORT = 587
 #        },
 #    }
 #}
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
